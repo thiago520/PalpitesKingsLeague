7fdae6d77265cfb644575a8d56f35d4519574f94
Thiago <thiago520@gmail.com>
2025-10-24 16:24:22 -0300

    adicionado logos de todos os times

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -130,7 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("bf624466324d6f07")
+/******/ 		__webpack_require__.h = () => ("ec8850b3f27060b6")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
