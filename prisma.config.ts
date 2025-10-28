617203fd26cd9a4d3d8616e6df51e1d5a166a594
Thiago <thiago520@gmail.com>
2025-10-28 12:11:52 -0300

    att bug QL

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,5 +1,9 @@
 // prisma.config.ts
 import { defineConfig } from "prisma/config";
+import dotenv from "dotenv";
+
+dotenv.config(); // 👈 garante que .env seja carregado
+
 export default defineConfig({
   schema: "prisma/schema.prisma",
 });
